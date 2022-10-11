6598bb64fd8d8dac88595eaf0e728d7760ed845c
ebolblga <kkochanovskiy@gmail.com>
2022-10-11 04:45:10 +0300

    feat!: full CRUD finished

--- MySQL-WinForms-CRUD/DataAccess.cs
+++ MySQL-WinForms-CRUD/DataAccess.cs
@@ -1,15 +1,68 @@
-﻿using System.Data;
+﻿using System.Collections.Immutable;
+using System.Data;
 using Dapper;
 
 namespace MySQL_WinForms_CRUD;
 
 public class DataAccess
 {
+    // SELECT * WHERE
     public List<Person> GetPeople(int id)
     {
         using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("CrudDB")))
         {
             return connection.Query<Person>($"SELECT * FROM People WHERE id = '{id}'").ToList();
+        }
+    }
+
+    //INSERT
+    public void InsetPerson(int id, string Name, bool Vote)
+    {
+        List<Person> people = new List<Person>();
+        people.Add(new Person { id = id, Name = Name, Vote = Vote });
+
+        using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("CrudDB")))
+        {
+            connection.Execute($"INSERT INTO People(id, Name, Vote) VALUES (@id, @Name, @Vote)", people);
+        }
+    }
+
+    // SELECT *
+    public List<Person> GetAll()
+    {
+        using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("CrudDB")))
+        {
+            return connection.Query<Person>($"SELECT * FROM People").ToList();
+        }
+    }
+
+    // COUNT
+    public int Count()
+    {
+        using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("CrudDB")))
+        {
+            return connection.ExecuteScalar<int>($"SELECT COUNT(*) FROM People");
+        }
+    }
+
+    // DELETE
+    public void DeletePerson(int id)
+    {
+        using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("CrudDB")))
+        {
+            connection.ExecuteAsync($"DELETE People WHERE id = '{id}'");
+        }
+    }
+
+    // UPDATE
+    public void UpdatePerson(int id, string Name, bool Vote)
+    {
+        List<Person> people = new List<Person>();
+        people.Add(new Person { id = id, Name = Name, Vote = Vote });
+
+        using (IDbConnection connection = new System.Data.SqlClient.SqlConnection(Helper.CnnVal("CrudDB")))
+        {
+            connection.Execute($"UPDATE People SET Name = @Name, Vote = @Vote WHERE id = @id", people);
         }
     }
 }
--- MySQL-WinForms-CRUD/Form1.cs
+++ MySQL-WinForms-CRUD/Form1.cs
@@ -4,6 +4,7 @@ public partial class Form1 : Form
 {
     List<Person> people = new List<Person>();
 
+    // Prepares form
     public Form1()
     {
         InitializeComponent();
@@ -18,13 +19,140 @@ public partial class Form1 : Form
         listBox1.DisplayMember = "FullInfo";
     }
 
-    // Search button
+    // Search function
     private void button1_Click(object sender, EventArgs e)
     {
         DataAccess db = new DataAccess();
 
-        people = db.GetPeople(Convert.ToInt32(textBox1.Text));
+        int id;
 
+        try
+        {
+            id = Convert.ToInt32(textBox1.Text);
+        }
+        catch
+        {
+            MessageBox.Show("id value is not an int", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        people = db.GetPeople(id);
         UpdateList();
+
+        textBox1.Text = "";
+
+        if (people.Count == 0)
+        {
+            MessageBox.Show("Did not find person with such id", "Oops", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }    
+    }
+
+    // Insert function
+    private void button2_Click(object sender, EventArgs e)
+    {
+        DataAccess db = new DataAccess();
+
+        int id = db.Count() + 1;
+        bool vote;
+
+        try
+        {
+            vote = Convert.ToBoolean(textBox3.Text);
+        }
+        catch
+        {
+            MessageBox.Show("Vote value is not a bool", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        if (textBox2.Text == "")
+        {
+            MessageBox.Show("Name value is not a string", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        db.InsetPerson(id, textBox2.Text, vote);
+        UpdateList();
+
+        textBox2.Text = "";
+        textBox3.Text = "";
+    }
+
+    // Loads whole db
+    private void button3_Click(object sender, EventArgs e)
+    {
+        DataAccess db = new DataAccess();
+
+        people = db.GetAll();
+        UpdateList();
+
+        if (people.Count == 0)
+        {
+            MessageBox.Show("Database is empty", "Oops", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+    }
+
+    // Delete function
+    private void button4_Click(object sender, EventArgs e)
+    {
+        DataAccess db = new DataAccess();
+
+        int id;
+
+        try
+        {
+            id = Convert.ToInt32(textBox1.Text);
+        }
+        catch
+        {
+            MessageBox.Show("id value is not an int", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        db.DeletePerson(id);
+
+        textBox1.Text = "";
+    }
+
+    // Update function
+    private void button5_Click(object sender, EventArgs e)
+    {
+        DataAccess db = new DataAccess();
+
+        int id;
+        bool vote;
+
+        try
+        {
+            id = Convert.ToInt32(textBox6.Text);
+        }
+        catch
+        {
+            MessageBox.Show("id value is not an int", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        try
+        {
+            vote = Convert.ToBoolean(textBox4.Text);
+        }
+        catch
+        {
+            MessageBox.Show("Vote value is not a bool", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        if (textBox5.Text == "")
+        {
+            MessageBox.Show("Name value is not a string", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            return;
+        }
+
+        db.UpdatePerson(id, textBox5.Text, vote);
+        UpdateList();
+
+        textBox4.Text = "";
+        textBox5.Text = "";
+        textBox6.Text = "";
     }
 }
